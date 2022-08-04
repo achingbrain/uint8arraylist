5aa0c51d32dacd709f51550494aa951d84e8cf13
achingbrain <alex@achingbrain.net>
2022-08-04 09:45:47 +0100

    chore: add more benchmarks
    Also make it easy to run them in browsers and node

--- benchmarks/slice.js
+++ benchmarks/slice.js
@@ -1,33 +1,43 @@
+/* eslint-disable no-console */
+
+/*
+$ node benchmarks/slice.js
+$ npx playwright-test benchmarks/slice.js --runner benchmark
+*/
+
+import Benchmark from 'benchmark'
 import BufferList from 'bl/BufferList.js'
 import { Uint8ArrayList } from '../dist/src/index.js'
 
-const REPEAT = 10000
-let start = Date.now()
+const suite = new Benchmark.Suite()
 
-for (let i = 0; i < REPEAT; i++) {
-  const buf = new BufferList()
+suite
+  .add('slice BufferList', () => {
+    const buf = new BufferList()
 
-  for (let j = 0; j < REPEAT; j++) {
-    buf.append(Uint8Array.from([i, j, 1, 2, 3, 4, 5]))
-    buf.append(Uint8Array.from([i, j, 1, 2, 3, 4, 5]))
+    for (let j = 0; j < 10; j++) {
+      buf.append(Uint8Array.from([j, 1, 2, 3, 4, 5]))
+    }
+
     buf.slice()
-    buf.consume(buf.length)
-  }
-}
+  })
+  .add('slice Uint8ArrayList', () => {
+    const buf = new Uint8ArrayList()
 
-console.info('slice BufferList', Date.now() - start, 'ms') // eslint-disable-line no-console
+    for (let j = 0; j < 10; j++) {
+      buf.append(Uint8Array.from([j, 1, 2, 3, 4, 5]))
+    }
 
-start = Date.now()
-
-for (let i = 0; i < REPEAT; i++) {
-  const buf = new Uint8ArrayList()
-
-  for (let j = 0; j < REPEAT; j++) {
-    buf.append(Uint8Array.from([i, j, 1, 2, 3, 4, 5]))
-    buf.append(Uint8Array.from([i, j, 1, 2, 3, 4, 5]))
     buf.slice()
-    buf.consume(buf.length)
-  }
-}
+  })
 
-console.info('slice Uint8ArrayList', Date.now() - start, 'ms') // eslint-disable-line no-console
+suite
+  // add listeners
+  .on('cycle', (event) => {
+    console.log(String(event.target))
+  })
+  .on('complete', function () {
+    console.log('Fastest is ' + this.filter('fastest').map('name'))
+  })
+  // run async
+  .run({ async: true })
